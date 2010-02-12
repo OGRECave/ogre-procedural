66c04c4f9a5e701662e6c713f7ad6daf982ce066
michael.broutin <michael.broutin@localhost>
2010-02-12 22:57:51 +0000

    - Implemented createTorus

--- include/ProceduralPrimitiveFactory.h
+++ include/ProceduralPrimitiveFactory.h
@@ -11,7 +11,8 @@ class ProceduralPrimitiveFactory
 
 	public:
 		ProceduralPrimitiveFactory(Ogre::SceneManager* sceneManager) : sceneMgr(sceneManager) {}
-				
+		
+		// "BASIC" PRIMITIVES
 		Ogre::MeshPtr createBox(const Ogre::String& name, float sizeX, float sizeY, float sizeZ, int numSegX, int numSegY, int numSegZ, float uTile, float vTile);
 		Ogre::MeshPtr createCone(const Ogre::String& name, float radius, float height, int numSegHeight, int numSegBase, float uTile, float vTile);
 		Ogre::MeshPtr createSphere(const Ogre::String& name, float r, int nRings, int nSegments, bool normals, int numTexCoordSet, float uTile, float vTile);
@@ -19,8 +20,7 @@ class ProceduralPrimitiveFactory
 		Ogre::MeshPtr createIsoSphere(const Ogre::String& name, float radius, int seg);
 		Ogre::MeshPtr createCylinder(const Ogre::String& name, float radius, float height, bool capped, int numSegHeight, int numSegBase, float uTile, float vTile);
 		Ogre::MeshPtr createTube(const Ogre::String& name, float innerRadius, float outerRadius, float height, int numSegHeight, int numSegBase, float uTile, float vTile);
-		//TODO : implement
-		Ogre::MeshPtr createTorus(const Ogre::String& name, float innerRadius, float outerRadius);
+		Ogre::MeshPtr createTorus(const Ogre::String& name, float radius, float sectionRadius, int numSegCircle, int numSegSection, float uTile, float vTile);
 		//TODO : implement
 		Ogre::MeshPtr createPyramid(const Ogre::String& name, float size, float height);
 		//TODO : implement
@@ -28,10 +28,9 @@ class ProceduralPrimitiveFactory
 		//TODO : implement
 		Ogre::MeshPtr createPlane(const Ogre::String& name, const Ogre::Plane& plane, int numTexCoordSet=1, int numSegX=1, int numSegY=1, float uTile=1.0f, float vTile=1.0f);
 		
+		// EXTENDED PRIMITIVES
 		//TODO : implement
 		Ogre::MeshPtr createCapsule(const Ogre::String& name, float height, float radius, int numseg);
-		
-		
-		
-
+		//TODO : implement
+		Ogre::MeshPtr createChamferBox(const Ogre::String& name, float sizeX, float sizeY, float sizeZ, float sizeChamfer, int numSegX, int numSegY, int numSegZ, float uTile, float vTile);
 };
--- src/ProceduralPrimitiveFactory.cpp
+++ src/ProceduralPrimitiveFactory.cpp
@@ -399,3 +399,51 @@ Ogre::MeshPtr ProceduralPrimitiveFactory::createTube(const Ogre::String& name, f
    	}
 	return mesh;
 }
+
+Ogre::MeshPtr ProceduralPrimitiveFactory::createTorus(const Ogre::String& name, float radius, float sectionRadius, int numSegCircle, int numSegSection, float uTile, float vTile)
+{
+	Ogre::ManualObject * manual = sceneMgr->createManualObject(name);
+	manual->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_TRIANGLE_LIST);
+
+	float deltaSection = (Ogre::Math::TWO_PI / numSegSection);
+	float deltaCircle = (Ogre::Math::TWO_PI / numSegCircle);
+
+	int verticeIndex=0;
+	for (int i = 0; i <=numSegCircle; i++)
+		for (int j = 0; j<=numSegSection; j++)
+		{
+			Ogre::Vector3 c0(radius, 0.0, 0.0);
+			Ogre::Vector3 v0(radius+sectionRadius * cosf(j*deltaSection),sectionRadius * sinf(j*deltaSection),0.0);
+			Ogre::Quaternion q;
+			q.FromAngleAxis(Ogre::Radian(i*deltaCircle),Ogre::Vector3::UNIT_Y);
+			Ogre::Vector3 v = q * v0;
+			Ogre::Vector3 c = q * c0;
+			manual->position(v);
+			manual->normal((v-c).normalisedCopy());
+			manual->textureCoord(i/(float)numSegCircle*uTile, j/(float)numSegSection*vTile);
+
+			if (i != numSegCircle) 
+			{
+				manual->index(verticeIndex + numSegSection + 1);
+				manual->index(verticeIndex);               
+				manual->index(verticeIndex + numSegSection);
+				manual->index(verticeIndex + numSegSection + 1);
+				manual->index(verticeIndex + 1);
+				manual->index(verticeIndex);		
+			}
+			verticeIndex ++;
+		}	
+
+	manual->end();
+	Ogre::MeshPtr mesh = manual->convertToMesh(name);
+	float r = radius;
+	mesh->_setBounds( Ogre::AxisAlignedBox( Ogre::Vector3(-r, -r, -r), Ogre::Vector3(r, r, r) ), false );
+
+	mesh->_setBoundingSphereRadius(r);
+       unsigned short src, dest;
+	   if (!mesh->suggestTangentVectorBuildParams(Ogre::VES_TANGENT, src, dest))
+   	{
+		mesh->buildTangentVectors(Ogre::VES_TANGENT, src, dest);
+   	}
+	return mesh;
+}
