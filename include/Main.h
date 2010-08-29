56fbe348d69f4143867723944d43a64d088c0072
Mike <michael.broutin@gmail.com>
2010-08-29 23:31:46 +0200

    Added licence notice

--- include/Main.h
+++ include/Main.h
@@ -1,3 +1,30 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of ogre-procedural
+
+For the latest info, see http://code.google.com/p/ogre-procedural/
+
+Copyright (c) 2010 Michael Broutin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 #include "Ogre.h"
@@ -57,8 +84,9 @@ public:
         putMesh("capsule", "capsuleMesh", Vector3(0,10,10));
         Procedural::TorusKnotGenerator().realizeMesh("torusKnotMesh");
         putMesh("torusKnot", "torusKnotMesh", Vector3(-10,10,10));
-        Procedural::IcoSphereGenerator().realizeMesh("icoSphereMesh");
-        putMesh("icosphere", "icoSphereMesh", Vector3(10,10,10));
+        Procedural::IcoSphereGenerator().setRadius(5.).setNumIterations(3).realizeMesh("icoSphereMesh");
+        putMesh("icosphere2", "icoSphereMesh", Vector3(10,10,10));
+        //putMesh("icosphere", "geosphere8000.mesh", Vector3(10,10,10));
     }
 
     void putMesh2(const std::string& entityName, const std::string& meshName, const Vector3& position = Vector3::ZERO)
--- include/Precompiled.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "Ogre.h"
-#include "OIS.h"
