fa2ca292fb02c946a0b26f8e99a4999b8672a0a8
michael.broutin <michael.broutin@localhost>
2011-03-27 19:29:52 +0200

    Tweaked BaseApplication to not show the config dialog when not needed.

--- samples/common/src/BaseApplication.cpp
+++ samples/common/src/BaseApplication.cpp
@@ -53,7 +53,7 @@ bool BaseApplication::configure(void)
     // Show the configuration dialog and initialise the system
     // You can skip this and use root.restoreConfig() to load configuration
     // settings if you were sure there are valid ones saved in ogre.cfg
-    if(mRoot->showConfigDialog())
+	if(mRoot->restoreConfig() || mRoot->showConfigDialog())
     {
         // If returned true, user clicked OK so initialise
         // Here we choose to let the system create a default rendering window by passing 'true'
