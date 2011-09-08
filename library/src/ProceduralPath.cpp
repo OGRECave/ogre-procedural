6e6ddbe54c8140f6775211e820da984efd575fdd
michael.broutin <michael.broutin@localhost>
2011-09-08 16:09:35 +0200

    Added a few convenient functions to Shape and Path : inserPoint, reverse, appendXXX and extractSubXXX.

--- library/src/ProceduralPath.cpp
+++ library/src/ProceduralPath.cpp
@@ -32,36 +32,7 @@ THE SOFTWARE.
 using namespace Ogre;
 
 namespace Procedural
-{/*
-	void Path::fixSharpAngles(Ogre::Real radius)
-	{
-		Ogre::Vector3 lastV0 = mPoints[0];
-		Quaternion q;
-		Quaternion lastQ = Utils::_computeQuaternion(getAvgDirection(0));
-		for (unsigned int i=1;i<mPoints.size();i++)
-		{	
-			Quaternion q = Utils::_computeQuaternion(getAvgDirection(i));
-			Ogre::Vector3& v0 = mPoints[i];
-			Plane plane1(lastQ * Vector3::UNIT_Z, lastV0);
-			Plane plane2(q * Vector3::UNIT_Z, v0);
-			Line inter;
-			if (plane1.intersect(plane2, inter))
-			{
-				Vector3 v = inter.shortestPathToPoint(v0);
-				if (v.length() < radius)
-				{
-					v0 = v0 + (radius-v.length()) * v.normalisedCopy();
-					v = inter.shortestPathToPoint(lastV0);
-					lastV0+= (radius-v.length()) * v.normalisedCopy();
-					mPoints[i-1] = lastV0;
-				}
-			}
-
-			lastQ = q;
-			lastV0 = v0;
-		}
-	}*/
-
+{
 	Path Path::mergeKeysWithTrack(const Track& track)
 	{
 		if (!track.isInsertPoint() || track.getAddressingMode() == Track::AM_POINT)
