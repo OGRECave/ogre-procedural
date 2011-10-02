3c91660643406201e1aa6291ddce6d512b37a847
michael.broutin <michael.broutin@localhost>
2011-10-02 13:49:10 +0200

    In Visual Studio, logging now also outputs to console.

--- library/include/ProceduralUtils.h
+++ library/include/ProceduralUtils.h
@@ -42,6 +42,9 @@ public:
 	static void log(const Ogre::String& st)
 	{
 		Ogre::LogManager::getSingleton().logMessage("[PROCEDURAL] " + st);
+	#if (PROCEDURAL_PLATFORM == PROCEDURAL_PLATFORM_WIN32)
+		OutputDebugString((st + "\n").c_str());
+	#endif
 	}
 
 	/// Gets the min of the coordinates between 2 vectors
