e8e70a4c8b33f96fdf1434e8b4e4c17d9777d66a
mikarnage <michael.broutin@gmail.com>
2013-08-13 16:33:22 +0200

    Updated copyright notice

--- library/src/ProceduralTrack.cpp
+++ library/src/ProceduralTrack.cpp
@@ -4,7 +4,7 @@ This source file is part of ogre-procedural
 
 For the latest info, see http://www.ogreprocedural.org
 
-Copyright (c) 2010-2012 Michael Broutin
+Copyright (c) 2010-2013 Michael Broutin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
